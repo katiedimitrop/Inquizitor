0c0655cb26880cdaacc5a4de1f479708751e8ec8
katdm <catherine.dimitropoulaki@student.manchester.ac.uk>
2018-02-08 17:45:36 +0000

    js that saves input in variables

--- create-page.js
+++ create-page.js
@@ -1,24 +1,20 @@
+//Create array to hold questions
+//var questions[10] =  [];
+
 function takeInput()
 {
-  var question1 = document.getElementById("question1").value;
-  var question2 = document.getElementById("question2").value;
-  var question3 = document.getElementById("question3").value;
-  var question4 = document.getElementById("question4").value;
-  var question5 = document.getElementById("question5").value;
-  var question6 = document.getElementById("question6").value;
-  var question7 = document.getElementById("question7").value;
-  var question8 = document.getElementById("question8").value;
-  var question9 = document.getElementById("question9").value;
-  var question10 = document.getElementById("question10").value;
-  var question11= document.getElementById("question11").value;
-  var question12= document.getElementById("question12").value;
-  var question13 = document.getElementById("question13").value;
-  var question14 = document.getElementById("question14").value;
-  var question15 = document.getElementById("question15").value;
-  var question16 = document.getElementById("question16").value;
-  var question17 = document.getElementById("question17").value;
-  var question18 = document.getElementById("question18").value;
-  var question19 = document.getElementById("question19").value;
-  var question20 = document.getElementById("question20").value;
+  //Array not working so far..
+  /*for (questionId = 1; questionId <= 10; questionId++)
+  {
+    //Create string that refers to Id
+    var question = "q"+ questionId;
+
+    //Insert new question into array
+    questions[questionId - 1] = document.getElementById("question").value;
+
+    //var answer1 = document.getElementById("a1").value;
+  }*/
+  //Takes input of first text area and displays on screen when submit is pressed
+  var question1 = document.getElementById("q1").value;
   window.alert(question1);
 }
