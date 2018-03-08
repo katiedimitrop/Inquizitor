1b80302a686d862b0188cced332dc93b9cba0d5a
Kabir Roshan <kabir.roshan@student.manchester.ac.uk>
2018-03-08 13:57:33 +0000

    play.php for the players

---
+++ play.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+
+<head>
+  <link rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet"
+      href="https://code.getmdl.io/1.3.0/material.indigo-orange.min.css" />
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <link rel="stylesheet" href="styles/styles.css">
+
+</head>
+
+<body>
+     <div class="topnav">
+       <a href="contactPage.html">Contact</a>
+       <a href="loginPage.html">Log in</a>
+       <a href="connectPage.html">Connect</a>
+       <a href="hostPage.html">Host</a>
+       <a href="create-page.html">Create</a>
+       <a href="index.html">Home</a>
+       <img id="logo2" width="5%" height="5%" src="images\Logo2.png"/>
+       <header href="#">InnQUIZitor</header>
+     </div>
+
+</body>
+
+<?php
+
+?>
+
+</html>
