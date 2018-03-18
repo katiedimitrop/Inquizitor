b3e2f8b1f2c68817f28ea5d0a85d3141a16f23d3
Baptiste <baptiste.sagna@student.manchester.ac.uk>
2018-03-18 19:07:09 +0000

    new css

--- connectPage.php
+++ connectPage.php
@@ -56,11 +56,13 @@
 
     <div id="sessionKey">
           <h1>Join a session!</h1>
+          <div class="container">
           <div class="field-wrap">
             <form>
               <input type="radio" name="isLeader" value=1 checked> Team Leader<br>
               <input type="radio" name="isLeader" value=0> Team Member<br>
             </form>
+          </div>
           </div>
           <form action="/getArrayForPlayers.php" method="post">
 
