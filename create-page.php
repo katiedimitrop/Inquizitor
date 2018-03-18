47ca83b79a1c26d7041cc1c82ace4b0f3d5904ac
katdm <catherine.dimitropoulaki@student.manchester.ac.uk>
2018-03-18 15:54:46 +0000

    js changes

--- create-page.php
+++ create-page.php
@@ -208,11 +208,11 @@ input:focus ~ .highlight {
 <body>
 <div class="topnav">
   <a href="contactPage.html">Contact</a>
-  <a href="loginPage.html">Log in</a>
-	<a href="connectPage.html">Connect</a>
-  <a href="hostPage.html">Host</a>
-  <a href="create-page.html">Create</a>
-  <a href="index.html">Home</a>
+  <a href="Logout.php">Log out</a>
+	<a href="connectPage.php">Connect</a>
+  <a href="hostPage.php">Host</a>
+  <a href="node">Create</a>
+  <a href="index.php">Home</a>
   <img id="logo2" width="5%" height="5%" src="/Logo2.png"/>
   <header href="#">InnQUIZitor</header>
 </div>
