20ba8bbfb9b9b96289aba2b75a850bd5b3b7ae59
Baptiste <baptiste.sagna@student.manchester.ac.uk>
2018-03-21 14:45:29 +0000

    cleanup

---
+++ Test Files/test2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+
+<head>
+  <link rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet"
+      href="https://code.getmdl.io/1.3.0/material.indigo-orange.min.css" />
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <link rel="stylesheet" href="styles/styles.css">
+
+</head>
+
+<body>
+     <div class="topnav">
+       <a href="contactPage.html">Contact</a>
+       <a href="loginPage.html">Log in</a>
+       <a href="connectPage.html">Connect</a>
+       <a href="hostPage.html">Host</a>
+       <a href="node">Create</a>
+       <a href="index.html">Home</a>
+       <img id="logo2" width="5%" height="5%" src="images\Logo2.png"/>
+       <header href="#">InnQUIZitor</header>
+     </div>
+
+</body>
+
+<?php
+
+# connecting to the database
+
+$dbuser = 'master5';
+$dbpass = 'master123';
+$dbhost = 'projectdatabase3.cpvnf88ap5ww.eu-west-2.rds.amazonaws.com';
+$dbname = "projectdatabase3";
+
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to Connect to '$dbhost'");
+
+# query will select all questions from Question table
+$questionQuery = "SELECT questionText FROM projectdatabase3.Question";
+
+# storing the results of the query
+$result = $connect->query($questionQuery);
+
+# displaying the questions
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Question: " . $row["questionText"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$connection->close();
+?>
+
+</html>
